dcbeb67e13768308ee79de04f0c2f424dcf07a65
Ohm Jodhani <ohm.jodhani@dynamicelements.no>
2020-04-01 11:56:20 +0530

    Updated fetching container logic.

--- Service/TenantStore.Service.CosmosDB/TenantStoreService .cs
+++ Service/TenantStore.Service.CosmosDB/TenantStoreService .cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using Constants;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,13 +13,8 @@ namespace TenantStore.Service.CosmosDB
 {
     public class TenantStoreService : ITenantStoreService
     {
-
-        private readonly string databaseId = "TenantStore";
         private readonly CosmosClient cosmosClient;
         private readonly ILogger<TenantStoreService> logger;
-        private readonly string containerId = "Inventory";
-        private Database database;
-        private Container container;
 
         public TenantStoreService(CosmosClient cosmosClient, ILogger<TenantStoreService> logger)
         {
@@ -28,35 +24,35 @@ namespace TenantStore.Service.CosmosDB
 
         public async Task CreateTenantAsync(Tenant tenant)
         {
-            await CreateDatabaseAndContainerAsync();
-            ItemResponse<Tenant> tenantResponse = await this.container.CreateItemAsync<Tenant>(tenant, new PartitionKey(tenant.region));
+            var container = GetContainer();
+            ItemResponse<Tenant> tenantResponse = await container.CreateItemAsync<Tenant>(tenant, new PartitionKey(tenant.region));
         }
 
         public async Task UpdateTenantAsync(Tenant tenant)
         {
-            await CreateDatabaseAndContainerAsync();
+            var container = GetContainer();
             await container.UpsertItemAsync<Tenant>(tenant);
         }
 
         public async Task DeleteTenantAsync(Tenant tenant)
         {
-            await CreateDatabaseAndContainerAsync();
+            var container = GetContainer();
             await container.DeleteItemAsync<Tenant>(tenant.id.ToString(), new PartitionKey(tenant.region));
         }
 
         public async Task<Tenant> GetTenantAsync(Guid id)
         {
-            await CreateDatabaseAndContainerAsync();
-            var sqlQueryText = $"SELECT * FROM c where c.id = @id";
-            var parameters = new Dictionary<string, string>();
-            parameters.Add("@id", id.ToString());
+            var sqlQueryText = "SELECT c.id, c.name, c.region, c.metadata FROM c where c.id = @id";
+            var parameters = new Dictionary<string, string>
+            {
+                { "@id", id.ToString() }
+            };
             return (await FetchTenants(sqlQueryText, parameters)).FirstOrDefault();
         }
 
         public async Task<List<Tenant>> GetAllTenantsAsync()
         {
-            await CreateDatabaseAndContainerAsync();
-            var sqlQueryText = "SELECT * FROM c";
+            var sqlQueryText = "SELECT c.id, c.name, c.region, c.metadata FROM c";
             return await FetchTenants(sqlQueryText);
         }
 
@@ -74,7 +70,8 @@ namespace TenantStore.Service.CosmosDB
                     }
                 }
 
-                FeedIterator<Tenant> queryResultSetIterator = this.container.GetItemQueryIterator<Tenant>(queryDefinition);
+                var container =  GetContainer();
+                FeedIterator<Tenant> queryResultSetIterator = container.GetItemQueryIterator<Tenant>(queryDefinition);
                 List<Tenant> tenants = new List<Tenant>();
 
                 while (queryResultSetIterator.HasMoreResults)
@@ -94,19 +91,10 @@ namespace TenantStore.Service.CosmosDB
             return null;
         }
 
-        private async Task CreateDatabaseAndContainerAsync()
+        private Container GetContainer()
         {
-            // Create a new database
-            this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
-            Console.WriteLine("Created Database: {0}\n", this.database.Id);
-            await CreateContainerAsync();
-        }
-
-        private async Task CreateContainerAsync()
-        {
-            // Create a new container
-            this.container = await this.database.CreateContainerIfNotExistsAsync(containerId, "/region", 400);
-            Console.WriteLine("Created Container: {0}\n", this.container.Id);
+            Database database = this.cosmosClient.GetDatabase(CosmosKeys.DatabaseId);
+            return database.GetContainer(CosmosKeys.ContainerId);
         }
     }
 }
